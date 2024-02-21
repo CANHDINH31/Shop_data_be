69f9f0ff3b6f2dcfc1b4571f8a3e2d2b66e30795
dinhphamcanh <dinhpc@solashi.com>
2024-02-21 09:35:24 +0700

    feat: set port

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/docs', app, document);
 
-  const PORT = process.env.PORT || 5000;
+  const PORT = 500;
 
   await app.listen(PORT);
 }
