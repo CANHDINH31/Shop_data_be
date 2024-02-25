b4906b8f6d3b111492ae3874419923d1822c8a18
dinhphamcanh <dinhphamcanh@gmail.com>
2024-02-25 21:53:01 +0700

    feat: key api

--- src/keys/keys.service.ts
+++ src/keys/keys.service.ts
@@ -187,7 +187,8 @@ export class KeysService {
     }
   }
 
-  @Cron(CronExpression.EVERY_DAY_AT_1PM)
+  // @Cron(CronExpression.EVERY_DAY_AT_1PM)
+  @Cron(CronExpression.EVERY_5_SECONDS)
   async checkExpiredKey() {
     try {
       console.log('start cron check expire key');
