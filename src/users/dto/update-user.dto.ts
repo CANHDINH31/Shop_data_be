440ee6f6abe375cebc1ce9d2be4f637d1b652060
dinhphamcanh <dinhphamcanh@gmail.com>
2024-03-05 23:03:24 +0700

    fix: api update user

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,8 +1,6 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-user.dto';
 import { IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
+export class UpdateUserDto {
   @IsOptional()
   @IsString()
   password?: string;
