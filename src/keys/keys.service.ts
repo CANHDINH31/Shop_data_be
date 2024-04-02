5fb75f5230391c723ed090f821f8f11d6d88dc20
dinhphamcanh <dinhpc@solashi.com>
2024-04-02 09:10:29 +0700

    fix: update time cronjob key

--- src/keys/keys.service.ts
+++ src/keys/keys.service.ts
@@ -451,7 +451,7 @@ export class KeysService {
     }
   }
 
-  @Cron(CronExpression.EVERY_DAY_AT_1PM)
+  @Cron(CronExpression.EVERY_DAY_AT_1AM)
   async checkExpiredKey() {
     try {
       console.log('start cron check expire key');
